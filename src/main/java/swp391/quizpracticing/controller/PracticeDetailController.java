b31fba3388806bf410564b4bb321a53aeb9c94b8
AnhQuan2208 <120321862+aq2208@users.noreply.github.com>
2023-06-08 15:34:55 +0700

    revert to past

--- src/main/java/swp391/quizpracticing/controller/PracticeDetailController.java
+++ src/main/java/swp391/quizpracticing/controller/PracticeDetailController.java
@@ -17,9 +17,4 @@ public class PracticeDetailController {
         model.addAttribute("id", id);
         return "practice_detail/practice_detail";
     }
-
-    @GetMapping("/new-practice-detail")
-    public String getToNewPracticeDetail() {
-        return "practice_detail/practice_detail";
-    }
 }
